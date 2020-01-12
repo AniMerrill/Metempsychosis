67ec44c7d78cf92fd63f68ef9817e7cdcce1285c
BenjaminK1ng <kasolobenjamin@gmail.com>
2020-01-12 12:40:40 +0200

    Player and map

---
+++ map.gd
@@ -0,0 +1,19 @@
+extends Navigation2D
+
+onready var _player = $"../player"
+
+var target_position = Vector2()
+var path = []
+
+func _input(event):
+	if (event.is_action_pressed("click")):
+		target_position = event.position
+		_update_path(_player.get_position(), target_position)
+	else:
+		return
+	
+func _update_path(start_position, target_position):
+	path = get_simple_path(start_position, target_position)
+	path.remove(0)
+	_player.target = path
+	_player.set_process(true)
