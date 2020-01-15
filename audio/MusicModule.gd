fd09550dd79bf2a86d864322d7af2ed73100a2f0
thijsengel <45845033+thijsengel@users.noreply.github.com>
2020-01-15 20:31:04 +0100

    forgot topush sound files

---
+++ audio/MusicModule.gd
@@ -0,0 +1,45 @@
+extends Node
+#To do
+#	- Edit audio files for smoother transitions
+#
+
+onready var mdm = $MixingDeskMusic
+
+onready var sfx = $SFX
+
+var current_song = "none"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#inititialise all the songs
+	mdm.init_song("Exploration")
+	#start with explore
+	mdm.play("Exploration")
+	current_song = "Exploration"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+#buttons for debug
+
+#guard appears
+func _on_Button_pressed():
+	mdm.queue_bar_transition("GuardOnWatch")
+	current_song = "GuardOnWatch"
+	print("queue guard")
+
+#Game over 
+#use queue_sequence() to loop smth once, then proceed to next song
+func _on_Button2_pressed():
+	mdm.queue_sequence(["GameOver", "Exploration"], "beat", "loop")
+	current_song = "Exploration"
+	print("queue game over")
+
+
+#Back to main loop
+func _on_Button3_pressed():
+	mdm.queue_bar_transition("Exploration")
+	current_song = "Exploration"
+	print("queue exploration")
+		
---
+++ audio/SFX.gd
@@ -0,0 +1,19 @@
+extends Node
+
+#sound module
+#call $SFX.play(sfxname:String) to play the sound. String name is the node name.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		play_sfx("Footsteps")
+		
+		
+
+func play_sfx(sfxname:String):
+  if has_node(sfxname):
+     get_node(sfxname).play()
