6fdd7389bf35da10e949d7b417dbe800a630ebba
thijsengel <45845033+thijsengel@users.noreply.github.com>
2020-01-16 22:34:47 +0100

    Added synth music

--- audio/MusicModule.gd
+++ audio/MusicModule.gd
@@ -12,15 +12,15 @@ var current_song = "none"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#inititialise all the songs
-	mdm.init_song("Exploration")
-	#start with explore
-	mdm.play("Exploration")
-	current_song = "Exploration"
+#	mdm.init_song("Exploration")
+#	#start with explore
+#	mdm.play("Exploration")
+#	current_song = "Exploration"
 	
 	#electronic theme
 	mdm.init_song("LevelTheme")
 	mdm.play("LevelTheme")
-
+	mdm.mute_below_layer("LevelTheme", 8)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
