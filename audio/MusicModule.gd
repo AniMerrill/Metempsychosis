dbf47c4235d8c4ff52fe08c41071812eb55c7ce7
thijsengel <45845033+thijsengel@users.noreply.github.com>
2020-01-17 17:05:11 +0100

    added fade in range functions

--- audio/MusicModule.gd
+++ audio/MusicModule.gd
@@ -5,8 +5,6 @@ extends Node
 
 onready var mdm = $MixingDeskMusic
 
-onready var sfx = $SFX
-
 var current_song = "none"
 
 # Called when the node enters the scene tree for the first time.
@@ -19,10 +17,8 @@ func _ready():
 	
 	#electronic theme
 	mdm.init_song("LevelTheme")
-	mdm.play("LevelTheme")
-	# NOTE: I had to comment this out to run the game, AniMerrill
-	mdm.start_alone("LevelTheme", 10)
-	
+	mdm.start_alone("LevelTheme", 8)
+	mdm.toggle_mute("LevelTheme", 7)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -32,21 +28,37 @@ func _process(delta):
 
 #guard appears
 func _on_Button_pressed():
-	mdm.queue_bar_transition("GuardOnWatch")
-	current_song = "GuardOnWatch"
-	print("queue guard")
+#	mdm.queue_bar_transition("GuardOnWatch")
+#	current_song = "GuardOnWatch"
+#	print("queue guard")
+	mdm.toggle_fade("LevelTheme", 9)
 
 #Game over 
 #use queue_sequence() to loop smth once, then proceed to next song
 func _on_Button2_pressed():
-	mdm.queue_sequence(["GameOver", "Exploration"], "beat", "loop")
-	current_song = "Exploration"
-	print("queue game over")
-
+#	mdm.queue_sequence(["GameOver", "Exploration"], "beat", "loop")
+#	current_song = "Exploration"
+#	print("queue game over")
+	_fadein_above_layer("LevelTheme", 6, 0)
 
 #Back to main loop
 func _on_Button3_pressed():
-	mdm.queue_bar_transition("Exploration")
-	current_song = "Exploration"
-	print("queue exploration")
+#	mdm.queue_bar_transition("Exploration")
+#	current_song = "Exploration"
+#	print("queue exploration")
+	_fadein_below_layer("LevelTheme", 9, 2) 
+	
+	print("sfddsf")
+	mdm.get_node("LevelTheme").get_child_count()
+
+
+#custom functions
+#fade below track up until last_track
+func _fadein_below_layer(song:String, track:int, last_track:int):
+	for i in range(track + last_track):
+		mdm.fade_in(song, i)
 		
+#fade above layer x
+func _fadein_above_layer(song:String, track:int, last_track:int):
+	for i in range(last_track + track):
+		mdm.fade_in(song, i)
