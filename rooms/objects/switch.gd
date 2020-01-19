f60528469224af901d1888b0635310afbbf04dc7
Bart van Delft <bartvandelft@gmail.com>
2020-01-19 16:30:02 +0100

    Update switch graphics.

--- rooms/objects/switch.gd
+++ rooms/objects/switch.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
+onready var xor_on = $xor_switch0000
+onready var xor_off = $xor_switch0001
+
 func _ready():
 	set_toggled(false)
 
 func set_toggled(toggled):
-	if toggled:
-		$ColorRect.color = Color.green
-	else:
-		$ColorRect.color = Color.red
+	xor_on.visible = not toggled
+	xor_off.visible = toggled
