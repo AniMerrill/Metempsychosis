b222118bb4178e496ef1681b0f50838c6bf5d475
AniMerrill <ethan@animerrill.com>
2020-01-19 19:34:42 -0500

    Finale Cutscene complete

--- rooms/final_room.gd
+++ rooms/final_room.gd
@@ -25,6 +25,7 @@ func _ready():
 	room.connect("object_clicked", self, "_on_object_clicked")
 	
 	# warning-ignore:return_value_discarded
+# warning-ignore:return_value_discarded
 	$EffectsOverlay/AnimationPlayer.connect(
 			"animation_finished", 
 			self, 
@@ -33,26 +34,6 @@ func _ready():
 	
 	# warning-ignore:return_value_discarded
 	Prompt.connect("responded", self, "_on_responded")
-	
-	# Set Live Feed Sprites
-	if GameState.current_player() == GameState.PLAYER.PLAYER_A:
-		# Neptonian
-		pass
-	else:
-		if GameState.set_state(GameState.STATE.PLAYER_A_TOOL_HAMMER, true):
-			$GenericRoom/Objects/TheMachine/Screen/Tool.visible = true
-			$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 48
-		if GameState.set_state(GameState.STATE.PLAYER_A_TOOL_SCREWDRIVER, true):
-			$GenericRoom/Objects/TheMachine/Screen/Tool.visible = true
-			$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 49
-		if GameState.set_state(GameState.STATE.PLAYER_A_TOOL_PISTOL, true):
-			$GenericRoom/Objects/TheMachine/Screen/Tool.visible = true
-			$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 50
-		if GameState.set_state(GameState.STATE.PLAYER_A_TOOL_PAINTBRUSH, true):
-			$GenericRoom/Objects/TheMachine/Screen/Tool.visible = true
-			$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 51
-		
-		
 	
 	if GameState.final_room_replay:
 		player.swap_rig()
@@ -70,14 +51,10 @@ func _on_object_clicked(node):
 		"TheMachine":
 			Timeout.stop_timer()
 			room.player_walk_to(machine.position)
-			# NOTE: I never set it to unfreeze anywhere, and I'm not sure when
-			# is most optimal since both options take control away from the player.
 			GameState.interaction_is_frozen = true
 			yield(player, "position_reached")
-			# TODO: Signal to play machine sfx in animation player?
 			$EffectsOverlay/AnimationPlayer.play("Activate")
 			
-			#start terminal sound effect
 			SoundModule.play_sfx("FinalRoom") 
 
 func _on_animation_finished(value : String):
@@ -163,8 +140,23 @@ func finale_cutscene():
 		FinaleState.LIVE_FEED:
 			RoomUtil.finale_dialog("live_feed")
 			yield(MessageDisplay, "messages_finished")
-			# TODO: Insert animation  trigger for live feed
-			# Change state there when finished
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play_backwards("Activate")
+			yield($GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer, "animation_finished")
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play("Activate")
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Loading")
+			$GenericRoom/Objects/TheMachine/Screen/ColorRect.visible = true
+			$GenericRoom/Objects/TheMachine/Screen/ColorRect/AnimationPlayer.play("Loading")
+			yield($GenericRoom/Objects/TheMachine/Screen/ColorRect/AnimationPlayer, "animation_finished")
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play_backwards("Activate")
+			yield($GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer, "animation_finished")
+			$GenericRoom/Objects/TheMachine/Screen/ColorRect.visible = false
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play("Activate")
+			
+			set_live_feed(true)
+			
 			finale_state = FinaleState.WARNING
 			finale_cutscene()
 		FinaleState.WARNING:
@@ -181,6 +173,13 @@ func finale_cutscene():
 						"Refuse"
 						)
 		FinaleState.THREAT:
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play_backwards("Activate")
+			yield($GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer, "animation_finished")
+			set_live_feed(false)
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play("Activate")
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Warning")
+			
 			RoomUtil.finale_dialog("threat")
 			yield(MessageDisplay, "messages_finished")
 			yield(get_tree().create_timer(0.5), "timeout")
@@ -193,8 +192,17 @@ func finale_cutscene():
 						"Refuse"
 						)
 		FinaleState.M_A_D:
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Warning_Glitch")
+			
 			RoomUtil.finale_dialog("mutually_assured_destruction")
 			yield(MessageDisplay, "messages_finished")
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play_backwards("Activate")
+			yield($GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer, "animation_finished")
+			
+			$GenericRoom/Objects/TheMachine/Screen/Block/AnimationPlayer.play("Activate")
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Death")
+			
 			GameState.opening_dialogue_is_outro = true
 			if GameState.final_room_replay:
 				var other_player = GameState.PLAYER.PLAYER_A \
@@ -207,3 +215,115 @@ func finale_cutscene():
 			MusicModule.state_changed("menu")
 			SoundModule.stop_sfx("FinalRoom")
 
+func set_info_screen():
+	if GameState.current_player() == GameState.PLAYER.PLAYER_A:
+		$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Plutonian")
+	else:
+		$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Neptonian")
+
+func set_live_feed(turn_on : bool):
+	if turn_on:
+		$GenericRoom/Objects/TheMachine/Screen/Mouth.visible = true
+		$GenericRoom/Objects/TheMachine/Screen/Hair.visible = true
+		$GenericRoom/Objects/TheMachine/Screen/Face.visible = true
+		$GenericRoom/Objects/TheMachine/Screen/Hat.visible = true
+		$GenericRoom/Objects/TheMachine/Screen/Tool.visible = true
+		
+		if GameState.current_player() == GameState.PLAYER.PLAYER_A:
+			$GenericRoom/Objects/TheMachine/Screen/Hair.frame = 0
+			
+			match GameState.custom_mouth(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.MOUTH.FROWN:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 43
+				GameState.PlayerRig.MOUTH.SMILE:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 44
+				GameState.PlayerRig.MOUTH.OPEN:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 45
+			
+			match GameState.custom_face(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.FACE.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 0
+				GameState.PlayerRig.FACE.BEARD:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 37
+				GameState.PlayerRig.FACE.MOUSTACHE:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 38
+			
+			match GameState.custom_hat(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.HAT.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 0
+				GameState.PlayerRig.HAT.BOWLER:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 34
+				GameState.PlayerRig.HAT.FEZ:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 35
+				GameState.PlayerRig.HAT.HELMET:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 36
+			
+			match GameState.custom_tool(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.TOOL.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 0
+				GameState.PlayerRig.TOOL.HAMMER:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 48
+				GameState.PlayerRig.TOOL.SCREWDRIVER:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 49
+				GameState.PlayerRig.TOOL.PISTOL:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 50
+				GameState.PlayerRig.TOOL.PAINTBRUSH:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 51
+			
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Neptonian_Wave")
+			
+		else:
+			$GenericRoom/Objects/TheMachine/Screen/Hair.frame = 0
+			
+			match GameState.custom_mouth(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.MOUTH.FROWN:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 40
+				GameState.PlayerRig.MOUTH.SMILE:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 41
+				GameState.PlayerRig.MOUTH.OPEN:
+					$GenericRoom/Objects/TheMachine/Screen/Mouth.frame = 42
+			
+			match GameState.custom_hair(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.HAIR.DOWN:
+					$GenericRoom/Objects/TheMachine/Screen/Hair.frame = 32
+				GameState.PlayerRig.HAIR.PONYTAIL:
+					$GenericRoom/Objects/TheMachine/Screen/Hair.frame = 33
+			
+			match GameState.custom_face(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.FACE.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 0
+				GameState.PlayerRig.FACE.BEARD:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 37
+				GameState.PlayerRig.FACE.MOUSTACHE:
+					$GenericRoom/Objects/TheMachine/Screen/Face.frame = 38
+			
+			match GameState.custom_hat(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.HAT.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 0
+				GameState.PlayerRig.HAT.BOWLER:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 34
+				GameState.PlayerRig.HAT.FEZ:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 35
+				GameState.PlayerRig.HAT.HELMET:
+					$GenericRoom/Objects/TheMachine/Screen/Hat.frame = 36
+			
+			match GameState.custom_tool(GameState.PLAYER.PLAYER_B):
+				GameState.PlayerRig.TOOL.NONE:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 0
+				GameState.PlayerRig.TOOL.HAMMER:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 48
+				GameState.PlayerRig.TOOL.SCREWDRIVER:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 49
+				GameState.PlayerRig.TOOL.PISTOL:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 50
+				GameState.PlayerRig.TOOL.PAINTBRUSH:
+					$GenericRoom/Objects/TheMachine/Screen/Tool.frame = 51
+			
+			$GenericRoom/Objects/TheMachine/Screen/Display/AnimationPlayer.play("Plutonian_Wave")
+			
+	else:
+		$GenericRoom/Objects/TheMachine/Screen/Mouth.visible = false
+		$GenericRoom/Objects/TheMachine/Screen/Hair.visible = false
+		$GenericRoom/Objects/TheMachine/Screen/Face.visible = false
+		$GenericRoom/Objects/TheMachine/Screen/Hat.visible = false
+		$GenericRoom/Objects/TheMachine/Screen/Tool.visible = false
