1df26665602a70fdb7c8130a04cd4d03816ed238
Bart van Delft <bartvandelft@gmail.com>
2020-01-28 13:51:59 +0100

    Disconnect the right signal from prompt.

--- menus/coop_switch_player.gd
+++ menus/coop_switch_player.gd
@@ -17,7 +17,7 @@ func _on_Button_pressed():
 
 func _on_continue_responded(response):
 	$Button.disabled = true
-	Prompt.disconnect("responded", self, "_on_coop_responded")
+	Prompt.disconnect("responded", self, "_on_continue_responded")
 	if response:
 		if me == 1:
 			GameState.set_current_player(GameState.PLAYER.PLAYER_B)
