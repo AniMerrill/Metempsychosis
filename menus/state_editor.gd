160f541db72c18bafb213792c7d4ece60e72fe54
Bart van Delft <bartvandelft@gmail.com>
2020-01-31 12:01:16 +0100

    Add button to Cheat Panel allowing to modify room tiles.

--- menus/state_editor.gd
+++ menus/state_editor.gd
@@ -11,21 +11,33 @@ onready var state_code_b = $StateEditor/Frame/StateCodeB
 onready var player_a = $StateEditor/Frame/PlayerA
 onready var player_b = $StateEditor/Frame/PlayerB
 onready var room_select = $StateEditor/Frame/RoomSelect
+onready var tile_popup = $StateEditor/Frame/TilesPopup
+onready var tile_options = $StateEditor/Frame/TilesPopup/Options
 
 var new_state
+var tile_state := [0,1,2,3,4,5,6,7,8]
 
 func _ready():
 	editor.visible = false
+	tile_popup.visible = false
+	
+	# Room selection
 	room_select.add_item("(In Menu)")
-	var index = 1
 	for item in WorldMap.prefix_map.keys():
 		room_select.add_item(item)
-		index += 1
+	
+	# Sliding tiles
+	for index in range(9):
+		for value in range(9):
+			tile_options.get_child(index).add_item(str(value))
+		tile_options.get_child(index).connect(
+				"item_selected", self, "on_tile_selected", [index])
 
 func _input(event):
 	if Input.is_action_just_pressed("cheat"):
 		editor.visible = not editor.visible
 		reload()
+
 
 func reload():
 	new_state = load('res://util/game_state.gd').new()
@@ -35,6 +47,7 @@ func reload():
 	_clear_node(custom_a_states)
 	_clear_node(custom_b_states)
 	
+	# Room selection
 	var index = 1
 	var selected_index = 0
 	for item in WorldMap.prefix_map.keys():
@@ -42,6 +55,10 @@ func reload():
 			selected_index = index
 		index += 1
 	room_select.select(selected_index)
+	
+	# Tile editing
+	tile_state = GameState.get_slide_puzzle_state()
+	update_tiles()
 	
 	var player_is_a = GameState.current_player() == GameState.PLAYER.PLAYER_A
 	player_a.pressed = player_is_a
@@ -51,6 +68,10 @@ func reload():
 		var state_container = other_states
 		if key == "CODE_CREATED_BY_PLAYER_A":
 			continue  # Handled in serialize function.
+		if key.begins_with("SLIDING_TILE_BIT"):
+			var is_set = GameState.get_state(GameState.STATE.get(key))
+			new_state.set_state(GameState.STATE.get(key), is_set)
+			continue  # Handled in separate window.
 		if key.begins_with("KEY_"):
 			state_container = key_states
 		if key.begins_with("PLAYER_A") and not key == "PLAYER_A_IN_XOR_POD":
@@ -65,6 +86,19 @@ func reload():
 		c.connect("pressed", self, "_on_toggled", [c, key])
 	_update_state_code()
 
+func update_tiles():
+	for index in range(9):
+		tile_options.get_child(index).select(tile_state[index])
+
+
+# Swap with the chosen value.
+func on_tile_selected(tile_value, tile_index):
+	print(tile_value)
+	var old_index = tile_state.find(tile_value)
+	var old_value = tile_state[tile_index]
+	tile_state[tile_index] = tile_value
+	tile_state[old_index] = old_value
+	update_tiles()
 
 func _clear_node(node):
 	for child in node.get_children():
@@ -95,6 +129,8 @@ func _on_ExitButton_pressed():
 func _on_SaveButton_pressed():
 	var code = ""
 	
+	new_state.set_slide_puzzle_state(tile_state)
+	
 	if player_a.pressed:
 		GameState.set_current_player(GameState.PLAYER.PLAYER_A)
 		code = new_state.serialize(GameState.PLAYER.PLAYER_A)
@@ -120,3 +156,16 @@ func _on_PlayerA_pressed():
 
 func _on_PlayerB_pressed():
 	player_a.pressed = not player_b.pressed
+
+
+func _on_TilesDoneButton_pressed():
+	tile_popup.visible = false
+
+
+func _on_TilesButton_pressed():
+	tile_popup.visible = true
+
+
+func _on_TilesResetButton_pressed():
+	tile_state = GameState.get_slide_puzzle_state()
+	update_tiles()
